5319d903bd9548ae3de98f31fea5b77c2ae75201
Patrick Allison <allison.122@osu.edu>
2018-05-18 10:06:13 -0400

    Update axi4lite_pb_bridge to v1.7.
    v1.7 pipelines the port access to make it easier to meet timing.
    That is, the port access is decoded in the cycle before write_strobe
    and k_write_strobe go. Because the port accesses are different
    for write_strobe and k_write_strobe (you check the top nybble for
    write_strobe, but do not for k_write_strobe), the module was changed
    to accept k_write_strobe directly.
    
    If k_write_strobe is not used in this module, just tie it to 0.
    Do not pass k_write_strobe if someone else uses it!
    
    Note that you *can* still use the constant space even if the base
    address has to be non-zero for some other reason. k_write_strobe
    only checks the low nybble. So if you set BASE_ADDRESS to 20h,
    you can write variable data to 20h, and constant data to 00h, and
    another module can write variable data to 00h. (I don't know why
    you would do this, but to each his own).
    
    Note that the intermediate versions here didn't work (they *almost* did...)
    hence the large version skip.

--- axi4lite_pb_bridge/src/axi4lite_pb_bridge.v
+++ axi4lite_pb_bridge/src/axi4lite_pb_bridge.v
@@ -12,13 +12,10 @@
 //% BASE_ADDR + 0x18 to BASE_ADDR + 0x1B: data[63:32] if DATA_WIDTH=64, otherwise read only shadow of data[31:00]
 //% BASE_ADDR + 0x1C to BASE_ADDR + 0x1F: addr[63:32] if ADDR_WIDTH>32, otherwise read only shadow of addr[31:00]
 //% 
-//% Note that if the address and data spaces are both 32-bits or less, only 16 ports are required.
+//% Note that if the address and data spaces are both 32-bits or less, only 16 ports are required and k_write_strobe can be used.
 //%
 //% NOTE: C_BASE_ADDRESS MUST BE ALIGNED ON A 32-BYTE BOUNDARY. This is enforced in the configuration GUI:
 //% i.e. only 0x20/40/60/80/A0/C0/E0 are allowed! This is still true even for address/data spaces 32-bits or less.
-//%
-//% However if you're really desparate you can cheat, strip off the address bit and AND it with the write strobe
-//% (e.g. wire [7:0] bridge_port_id = {port_id[7:5],1'b0,port_id[3:0]}; wire bridge_write_strobe = write_strobe && port_id[4]; )
 //%
 //% CONTROL[7:0]:
 //%        CONTROL[0]   = AXI_BEGIN    : Set to 1 to initiate an AXI transaction.
@@ -31,6 +28,7 @@
 module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH=32, parameter C_BASE_ADDRESS = 8'h00)(
         // PicoBlaze interface.
         input [7:0] port_id,
+        input k_write_strobe,
         input write_strobe,
         input read_strobe,
         input [7:0] out_port,
@@ -61,7 +59,7 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
         input m_axi_rvalid,
         output m_axi_rready        
     );
-
+    
     // AXI control register.
     reg axi_begin = 0;
     reg axi_wnr = 0;
@@ -82,13 +80,50 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
     wire [63:0] axi_data_expanded = { {(64-C_DATA_WIDTH){1'b0}}, axi_data};
     reg [C_ADDRESS_WIDTH-1:0] axi_address = {C_ADDRESS_WIDTH{1'b0}};
     wire [63:0] axi_address_expanded = { {(64-C_ADDRESS_WIDTH){1'b0}}, axi_address};
+
+    // ceil(C_DATA_WIDTH/8)
+    localparam N_DATA_BYTES = (C_DATA_WIDTH/8) + (((C_DATA_WIDTH % 8) == 0) ? 0 : 1 );
+    // ceil(C_ADDRESS_WIDTH/8). if ADDRESS_WIDTH=13, C_ADDRESS_WIDTH/8 = 1, C_ADDRESS_WIDTH % 8 = 5, so N_ADDRESS_BYTES = 2
+    // if C_ADDRESS_WIDTH=16, C_ADDRESS_WIDTH/8 = 2, C_ADDRESS_WIDTH % 8 = 0, so N_ADDRESS_BYTES=2
+    localparam N_ADDRESS_BYTES = (C_ADDRESS_WIDTH/8) + (((C_ADDRESS_WIDTH % 8) == 0) ? 0 : 1);
+    // Port select for data bytes (AND with axi_pb_write)
+    reg [N_DATA_BYTES-1:0] axi_data_waccess = {N_DATA_BYTES{1'b0}};
+    // k_write_strobe port select
+    reg [N_DATA_BYTES-1:0] axi_data_kwaccess = {N_DATA_BYTES{1'b0}};
+    // Port select for address bytes (AND with axi_pb_write)    
+    reg [N_ADDRESS_BYTES-1:0] axi_address_waccess = {N_ADDRESS_BYTES{1'b0}};
+    // k_write_strobe port select
+    reg [N_ADDRESS_BYTES-1:0] axi_address_kwaccess = {N_ADDRESS_BYTES{1'b0}};
+
+    wire [N_DATA_BYTES-1:0] axi_data_write_access;
+    wire [N_ADDRESS_BYTES-1:0] axi_address_write_access;
+
+    // Port select for control register (AND with axi_pb_write)
+    reg axi_control_waccess = 0;
+    // Port select for k_write_strobe. So the proper qualifier is (axi_control_waccess && axi_port_access && axi_write_strobe) || (axi_control_kwaccess && k_write_strobe)
+    reg axi_control_kwaccess = 0;
+    // AXI port access. This is for the full decode.    
+    reg axi_port_access = 0;
+    // AXI control port access.
+    wire axi_control_write_access = (axi_control_waccess && axi_port_access && write_strobe) || (axi_control_kwaccess && k_write_strobe);    
+
     
-    wire axi_port_access = (port_id[7:5] == C_BASE_ADDRESS[7:5]);
+    // So on the clock cycle before write_strobe/k_write_strobe,
+    // the port gets decoded, and axi_port_access goes high if needed,
+    // and axi_data_waccess/axi_address_waccess/axi_control_waccess go high.
+    // Then on the next clock, if it's write_strobe, it gets anded with axi_port_access, and if it's k_write_strobe there's no and.
+    // So we still have to pass through a LUT, but it shouldn't be as bad, I think.
+
     wire [4:0] axi_port_id = port_id[4:0];
     
     wire [7:0] picoblaze_register_map[31:0];
     
     always @(posedge m_axi_aclk) begin
+        // Port decodes
+        axi_port_access <= (port_id[7:5] == C_BASE_ADDRESS[7:5]);
+        axi_control_waccess <= (axi_port_id == 5'h00);
+        axi_control_kwaccess <= (axi_port_id[3:0] == 4'h0);
+
         // Begin read address transaction when axi_begin && !axi_wnr.
         // Assert ARVALID and wait for ARREADY.
         if (!m_axi_aresetn) axi_arvalid <= 0;
@@ -115,19 +150,19 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
         // Begin when bit 0 gets asserted.
         // It's software's job to make sure axi_begin never gets set again until axi_done goes high.
         if (!m_axi_aresetn) axi_begin <= 0;
-        else axi_begin <= out_port[0] && axi_port_access && (axi_port_id == 0) && write_strobe;
+        else axi_begin <= out_port[0] && axi_control_write_access;
         
         if (!m_axi_aresetn) axi_active <= 0;
-        else if (out_port[0] && axi_port_access && (axi_port_id == 0) && write_strobe) axi_active <= 1;
+        else if (out_port[0] && axi_control_write_access) axi_active <= 1;
         else if (axi_rready && m_axi_rvalid) axi_active <= 0;
         else if (axi_bready && m_axi_bvalid) axi_active <= 0;
         
         if (!m_axi_aresetn) axi_wnr <= 0;
-        else if (axi_port_access && (axi_port_id == 0) && write_strobe) axi_wnr <= out_port[1];
+        else if (axi_control_write_access) axi_wnr <= out_port[1];
         
         // Finish when rready & rvalid go high, or when bready && bvalid go high.
         if (!m_axi_aresetn) axi_done <= 0;
-        else if (axi_port_access && (axi_port_id == 0) && write_strobe) axi_done <= !out_port[0];
+        else if (axi_control_write_access) axi_done <= !out_port[0];
         else if (axi_rready && m_axi_rvalid) axi_done <= 1;
         else if (axi_bready && m_axi_bvalid) axi_done <= 1;
         
@@ -136,23 +171,56 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
     end        
 
     generate
-        genvar d_iter, a_iter;
-        for (d_iter=0;d_iter<C_DATA_WIDTH;d_iter=d_iter+1) begin : DL
-            always @(posedge m_axi_aclk) begin : DL_LOGIC
+        genvar da_iter, aa_iter, d_iter, a_iter;
+        // Generate the individual data byte write strobes.
+        // We can take 1 clock cycle for this.
+        for (da_iter=0;da_iter<N_DATA_BYTES;da_iter=da_iter+1) begin : DAL
+            always @(posedge m_axi_aclk) begin : DAL_LOGIC
                 // The addressing goes 8/9/10/11 -> 24/25/26/27, or 01000->01011 then 11000->11011.
                 // So remap by requiring axi_port_id[3:2]==2'b10,
                 // and requiring ({axi_port_id[4],axi_port_id[1:0]} == d_iter/8)
+                axi_data_waccess[da_iter] <= ((axi_port_id[3:2]==2'b10) && ({axi_port_id[4],axi_port_id[1:0]} == da_iter));                
+            end
+            if (da_iter<4) begin : LOW_DWORD_KWACCESS
+                always @(posedge m_axi_aclk) begin : KDAL_LOGIC
+                    axi_data_kwaccess[da_iter] <= ((axi_port_id[3:2]==2'b10) && (axi_port_id[1:0] == da_iter));
+                end
+            end else begin : HIGH_DWORD_KWACCESS
+                always @(posedge m_axi_aclk) begin : KDAL_LOGIC
+                    axi_data_kwaccess[da_iter] <= 0;
+                end
+            end            
+            assign axi_data_write_access[da_iter] = (axi_data_waccess[da_iter] && write_strobe) || (axi_data_kwaccess[da_iter] && k_write_strobe);
+        end
+        // Generate the individual address byte write strobes.
+        // We can take 1 clock cycle for this.
+        for (aa_iter=0;aa_iter<N_ADDRESS_BYTES;aa_iter=aa_iter+1) begin : AAL
+            always @(posedge m_axi_aclk) begin : AAL_LOGIC
+                // Remap as before, by requiring axi_port_id[3:2]==2'b11 now.
+                axi_address_waccess[aa_iter] <= ((axi_port_id[3:2]==2'b11) && ({axi_port_id[4],axi_port_id[1:0]} == aa_iter));
+            end
+            if (aa_iter<4) begin : LOW_DWORD_KWACCESS
+                always @(posedge m_axi_aclk) begin : KAAL_LOGIC
+                    axi_address_kwaccess[aa_iter] <= ((axi_port_id[3:2]==2'b11) && (axi_port_id[1:0] == aa_iter));
+                end
+            end else begin : HIGH_DWORD_KWACCESS
+                always @(posedge m_axi_aclk) begin : KAAL_LOGIC
+                    axi_address_kwaccess[aa_iter] <= 0;
+                end
+            end
+            assign axi_address_write_access[aa_iter] = (axi_address_waccess[aa_iter] && axi_port_access && write_strobe) || (axi_address_kwaccess[aa_iter] && k_write_strobe);
+        end
+        
+        for (d_iter=0;d_iter<C_DATA_WIDTH;d_iter=d_iter+1) begin : DL
+            always @(posedge m_axi_aclk) begin : DL_LOGIC
                 if (axi_rready && m_axi_rvalid) axi_data[d_iter] <= m_axi_rdata[d_iter];
-                else if (axi_port_access && (axi_port_id[3:2]==2'b10) && 
-                         ({axi_port_id[4],axi_port_id[1:0]} == d_iter/8) && write_strobe) 
-                         axi_data[d_iter] <= out_port[d_iter % 8];
+                else if (axi_data_write_access[d_iter/8]) axi_data[d_iter] <= out_port[d_iter % 8];
             end
         end
         for (a_iter=0;a_iter<C_ADDRESS_WIDTH;a_iter=a_iter+1) begin : AL
             always @(posedge m_axi_aclk) begin : AL_LOGIC
                 // Remap as before, by requiring axi_port_id[3:2]==2'b11.
-                if (axi_port_access && (axi_port_id[3:2]==2'b11) && 
-                    ({axi_port_id[4],axi_port_id[1:0]} == a_iter/8) && write_strobe) 
+                if (axi_address_write_access[a_iter/8])
                     axi_address[a_iter] <= out_port[a_iter % 8];
             end
         end
@@ -162,7 +230,7 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
         assign picoblaze_register_map[0] = AXI_CONTROL;
         assign picoblaze_register_map[16] = AXI_CONTROL;
         //% Spare space (shadow).
-        for (spare_iter=1;spare_iter<7;spare_iter=spare_iter+1) begin : SL
+        for (spare_iter=1;spare_iter<8;spare_iter=spare_iter+1) begin : SL
             //% 0x01-0x07 shadow 0x08-0x0F
             assign picoblaze_register_map[spare_iter] = picoblaze_register_map[spare_iter+8];
             //% 0x11-0x17 shadow 0x08-0x0F
@@ -176,7 +244,7 @@ module axi4lite_pb_bridge #(parameter C_ADDRESS_WIDTH=32, parameter C_DATA_WIDTH
             //% 64-bit expansion, or shadow 0x18-0x1F
             assign picoblaze_register_map[reg_iter+16] = (C_DATA_WIDTH>32) ? 
                 axi_data_expanded[(reg_iter-8+4)*8 +: 8] : axi_data_expanded[(reg_iter-8)*8 +: 8];
-            assign picoblaze_register_map[reg_iter+24] = (C_ADDRESS_WIDTH>32) ? 
+            assign picoblaze_register_map[reg_iter+20] = (C_ADDRESS_WIDTH>32) ? 
                 axi_address_expanded[(reg_iter-8+4)*8 +: 8] : axi_address_expanded[(reg_iter-8)*8 +: 8];
         end
     endgenerate
